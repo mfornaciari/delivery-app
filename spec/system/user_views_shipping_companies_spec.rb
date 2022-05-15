51288b4623c2de77d193aff84371dfb40ebc055f
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-15 18:36:44 -0300

    fix: adiciona domínio à tabela de transportadoras

--- spec/system/user_views_shipping_companies_spec.rb
+++ spec/system/user_views_shipping_companies_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Visitante acessa o índice de transportadoras' do
   it 'e vê as transportadoras cadastradas' do
     ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
-                            email_domain: '@express.com.br', registration_number: 28_891_540_000_121,
+                            email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
                             address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
     ShippingCompany.create!(brand_name: 'A Jato', corporate_name: 'A Jato S.A.',
-                            email_domain: '@ajato.com.br', registration_number: 19_824_380_000_107,
+                            email_domain: 'ajato.com', registration_number: 19_824_380_000_107,
                             address: 'Avenida B, 23', city: 'Natal', state: 'RN')
 
     visit root_path
@@ -16,18 +16,21 @@ describe 'Visitante acessa o índice de transportadoras' do
       within('#table_header') do
         expect(page).to have_content 'Nome fantasia'
         expect(page).to have_content 'Razão social'
+        expect(page).to have_content 'Domínio de e-mail'
         expect(page).to have_content 'CNPJ'
         expect(page).to have_content 'Endereço'
       end
       within('#Express') do
         expect(page).to have_content 'Express'
         expect(page).to have_content 'Express Transportes Ltda.'
+        expect(page).to have_content 'express.com.br'
         expect(page).to have_content '28.891.540/0001-21'
         expect(page).to have_content 'Avenida A, 10 - Rio de Janeiro/RJ'
       end
       within('#A_Jato') do
         expect(page).to have_content 'A Jato'
         expect(page).to have_content 'A Jato S.A.'
+        expect(page).to have_content 'ajato.com'
         expect(page).to have_content '19.824.380/0001-07'
         expect(page).to have_content 'Avenida B, 23 - Natal/RN'
       end
