a96e340dd068bc732ce2198297c9dd7721c56367
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-18 15:48:35 -0300

    refactor: simplifica código no VehiclesController

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,11 +2,11 @@ class VehiclesController < ApplicationController
   before_action :set_shipping_company
 
   def new
-    @vehicle = @shipping_company.vehicles.new
+    @vehicle = Vehicle.new
   end
 
   def create
-    @vehicle = @shipping_company.vehicles.new(vehicle_params)
+    @vehicle = Vehicle.new(vehicle_params)
     @vehicle.shipping_company = @shipping_company
     if @vehicle.save
       redirect_to @shipping_company, notice: 'Veículo cadastrado com sucesso.'
