fd6c8ce12eccaf9e003fd7bf1bd9931989a9528f
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-20 00:13:22 -0300

    feat: cria validações p/ intervalos de volume

--- app/models/volume_range.rb
+++ app/models/volume_range.rb
@@ -1,4 +1,23 @@
 class VolumeRange < ApplicationRecord
+  validates :min_volume, comparison: { greater_than_or_equal_to: 0 }
+  validates :max_volume, comparison: { greater_than_or_equal_to: 1 }
+  validate :not_previously_registered
+
   belongs_to :shipping_company
   has_many :weight_ranges
+
+  private
+
+  def not_previously_registered
+    return unless shipping_company
+
+    shipping_company.volume_ranges.each do |vrange|
+      if (vrange.min_volume..vrange.max_volume).include? min_volume
+        errors.add :min_volume, 'não pode estar contido em intervalos já registrados'
+      end
+      if (vrange.min_volume..vrange.max_volume).include? max_volume
+        errors.add :max_volume, 'não pode estar contido em intervalos já registrados'
+      end
+    end
+  end
 end
--- spec/models/volume_range_spec.rb
+++ spec/models/volume_range_spec.rb
@@ -1,4 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe VolumeRange, type: :model do
+  describe '#valid?' do
+    context 'presença' do
+      it 'falso quando transportadora está em branco' do
+        vrange = VolumeRange.new(shipping_company: nil)
+
+        vrange.valid?
+
+        expect(vrange.errors[:shipping_company]).to include 'é obrigatório(a)'
+      end
+    end
+
+    context 'valor' do
+      it 'falso quando volume mínimo está em branco, é < 0 ou está em intervalos já cadastrados' do
+        express = ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                          email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                          address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+        VolumeRange.create!(shipping_company: express, min_volume: 0, max_volume: 20)
+        express.reload
+        empty = VolumeRange.new(min_volume: '')
+        first_invalid = VolumeRange.new(min_volume: -1)
+        second_invalid = VolumeRange.new(shipping_company: express, min_volume: 0)
+        valid = VolumeRange.new(shipping_company: express, min_volume: 21, max_volume: 40)
+
+        [empty, first_invalid, second_invalid, valid].each(&:valid?)
+
+        expect(empty.errors[:min_volume]).to include 'não pode ficar em branco'
+        expect(first_invalid.errors[:min_volume]).to include 'deve ser maior ou igual a 0'
+        expect(second_invalid.errors[:min_volume]).to include(
+          'não pode estar contido em intervalos já registrados'
+        )
+        expect(valid.errors.include?(:min_volume)).to be false
+      end
+
+      it 'falso quando volume máximo está em branco, é < 1 ou está em intervalos já cadastrados' do
+        express = ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                          email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                          address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+        VolumeRange.create!(shipping_company: express, min_volume: 0, max_volume: 20)
+        express.reload
+        empty = VolumeRange.new(max_volume: '')
+        first_invalid = VolumeRange.new(max_volume: 0)
+        second_invalid = VolumeRange.new(shipping_company: express, max_volume: 20)
+        valid = VolumeRange.new(shipping_company: express, min_volume: 21, max_volume: 40)
+
+        [empty, first_invalid, second_invalid, valid].each(&:valid?)
+
+        expect(empty.errors[:max_volume]).to include 'não pode ficar em branco'
+        expect(first_invalid.errors[:max_volume]).to include 'deve ser maior ou igual a 1'
+        expect(second_invalid.errors[:max_volume]).to include(
+          'não pode estar contido em intervalos já registrados'
+        )
+        expect(valid.errors.include?(:max_volume)).to be false
+      end
+    end
+  end
 end
