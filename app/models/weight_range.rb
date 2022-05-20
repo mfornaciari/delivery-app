227e9c3592928ef65d93555045a8a043b5b7152e
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-20 10:06:26 -0300

    feat: cria validações p/ intervalo de peso

--- app/models/weight_range.rb
+++ app/models/weight_range.rb
@@ -1,3 +1,23 @@
 class WeightRange < ApplicationRecord
+  validates :min_weight, comparison: { greater_than_or_equal_to: 0 }
+  validates :max_weight, comparison: { greater_than: 0 }
+  validates :value, numericality: true
+  validate :not_previously_registered
+
   belongs_to :volume_range
+
+  private
+
+  def not_previously_registered
+    return unless volume_range
+
+    volume_range.weight_ranges.each do |wrange|
+      if (wrange.min_weight..wrange.max_weight).include? min_weight
+        errors.add :min_weight, 'não pode estar contido em intervalos já registrados'
+      end
+      if (wrange.min_weight..wrange.max_weight).include? max_weight
+        errors.add :max_weight, 'não pode estar contido em intervalos já registrados'
+      end
+    end
+  end
 end
--- spec/models/weight_range_spec.rb
+++ spec/models/weight_range_spec.rb
@@ -1,4 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe WeightRange, type: :model do
+  describe '#valid?' do
+    context 'Presença:' do
+      it 'Falso quando intervalo de volume está em branco' do
+        wrange = WeightRange.new(volume_range: nil)
+
+        wrange.valid?
+
+        expect(wrange.errors[:volume_range]).to include 'é obrigatório(a)'
+      end
+    end
+
+    context 'Formato:' do
+      it 'Falso quando valor está em branco ou não é numérico' do
+        first_invalid_range = WeightRange.new(value: '')
+        second_invalid_range = WeightRange.new(value: 'A')
+        valid_range = WeightRange.new(value: '50')
+
+        [first_invalid_range, second_invalid_range, valid_range].each(&:valid?)
+
+        expect(first_invalid_range.errors[:value]).to include 'não é um número'
+        expect(second_invalid_range.errors[:value]).to include 'não é um número'
+        expect(valid_range.errors.include?(:value)).to be false
+      end
+    end
+
+    context 'Valor:' do
+      it 'Falso quando peso mínimo está em branco ou é < 0' do
+        empty_range = WeightRange.new(min_weight: '')
+        invalid_range = WeightRange.new(min_weight: -1)
+        valid_range = WeightRange.new(min_weight: 0)
+
+        [empty_range, invalid_range, valid_range].each(&:valid?)
+
+        expect(empty_range.errors[:min_weight]).to include 'não pode ficar em branco'
+        expect(invalid_range.errors[:min_weight]).to include 'deve ser maior ou igual a 0'
+        expect(valid_range.errors.include?(:min_weight)).to be false
+      end
+
+      it 'Falso quando peso máximo está em branco ou é < 1' do
+        empty_range = WeightRange.new(max_weight: '')
+        invalid_range = WeightRange.new(max_weight: 0)
+        valid_range = WeightRange.new(max_weight: 1)
+
+        [empty_range, invalid_range, valid_range].each(&:valid?)
+
+        expect(empty_range.errors[:max_weight]).to include 'não pode ficar em branco'
+        expect(invalid_range.errors[:max_weight]).to include 'deve ser maior que 0'
+        expect(valid_range.errors.include?(:max_weight)).to be false
+      end
+    end
+
+    context 'Singularidade:' do
+      it 'Falso quando peso mínimo está incluso em intervalos já cadastrados' do
+        express = ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                          email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                          address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+        vrange = VolumeRange.create!(shipping_company: express, min_volume: 0, max_volume: 20)
+        WeightRange.create!(volume_range: vrange, min_weight: 0, max_weight: 10, value: 50)
+        invalid_range = WeightRange.new(volume_range: vrange, min_weight: 0)
+        valid_range = WeightRange.new(volume_range: vrange, min_weight: 11)
+
+        vrange.reload
+        [invalid_range, valid_range].each(&:valid?)
+
+        expect(invalid_range.errors[:min_weight]).to include('não pode estar contido em intervalos já registrados')
+        expect(valid_range.errors.include?(:min_weight)).to be false
+      end
+
+      it 'Falso quando peso máximo está incluso em intervalos já cadastrados' do
+        express = ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                          email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                          address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+        vrange = VolumeRange.create!(shipping_company: express, min_volume: 0, max_volume: 20)
+        WeightRange.create!(volume_range: vrange, min_weight: 0, max_weight: 10, value: 50)
+        invalid_range = WeightRange.new(volume_range: vrange, max_weight: 10)
+        valid_range = WeightRange.new(volume_range: vrange, max_weight: 11)
+
+        vrange.reload
+        [invalid_range, valid_range].each(&:valid?)
+
+        expect(invalid_range.errors[:max_weight]).to include('não pode estar contido em intervalos já registrados')
+        expect(valid_range.errors.include?(:max_weight)).to be false
+      end
+    end
+  end
 end
