2fef81b7ea68dccd549d5b1cf548d91d7352b004
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-22 14:06:52 -0300

    feat: cria link p/ registro de intervalos de prazo

---
+++ app/controllers/time_distance_ranges_controller.rb
@@ -0,0 +1,28 @@
+class TimeDistanceRangesController < ApplicationController
+  before_action :set_shipping_company
+
+  def new
+    @time_distance_range = TimeDistanceRange.new
+  end
+
+  def create
+    @time_distance_range = TimeDistanceRange.new(time_distance_range_params)
+    @time_distance_range.shipping_company = @shipping_company
+    if @time_distance_range.save
+      redirect_to @shipping_company, notice: 'Intervalo cadastrado com sucesso.'
+    else
+      flash.now[:notice] = 'Intervalo não cadastrado.'
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_shipping_company
+    @shipping_company = ShippingCompany.find(params[:shipping_company_id])
+  end
+
+  def time_distance_range_params
+    params.require(:time_distance_range).permit(:min_distance, :max_distance, :delivery_time)
+  end
+end
