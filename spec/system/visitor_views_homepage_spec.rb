d83514243ab250707d297babcbf6bfb91119face
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-25 05:52:37 -0300

    test: atualiza testes de página inicial Garante que usuário vê links de autenticação

--- spec/system/visitor_views_homepage_spec.rb
+++ spec/system/visitor_views_homepage_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 describe 'Visitante acessa a página inicial' do
-  it 'e vê o título da aplicação' do
+  it 'e vê os links de autenticação' do
     visit root_path
 
-    within('header') do
-      expect(page).to have_content 'SISTEMA DE GERENCIAMENTO DE ENTREGAS'
-    end
+    expect(page).to have_content 'SISTEMA DE GERENCIAMENTO DE ENTREGAS'
+    expect(page).to have_link 'Entrar (usuário)'
+    expect(page).to have_link 'Entrar (administrador)'
   end
 
   it 'e vê o menu de navegação' do
+    ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                            email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                            address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+
     visit root_path
 
     within('nav') do
       expect(page).to have_link 'Início'
+      expect(page).not_to have_link 'Transportadoras'
+      expect(page).not_to have_link 'Express'
+      expect(page).not_to have_button 'Sair'
     end
   end
 end
