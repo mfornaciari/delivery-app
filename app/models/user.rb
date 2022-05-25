1bd5e342c40bcdc9485fcd6517f961aa02067c82
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-24 21:47:22 -0300

    feat: cria validação para usuário Verifica se domínio de e-mail pertence a alguma transportadora

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,20 @@ class User < ApplicationRecord
 
   belongs_to :shipping_company
 
-  after_initialize :set_shipping_company
+  before_validation :set_shipping_company
+
+  validate :company_exists
 
   private
 
   def set_shipping_company
     domain = email.split('@')[1]
     self.shipping_company = ShippingCompany.find_by(email_domain: domain)
+  end
+
+  def company_exists
+    return if shipping_company
+
+    errors.add :email, 'não possui um domínio registrado'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'Valor:' do
+      it 'Falso quando domínio de e-mail não está registrado' do
+        ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+        ShippingCompany.create!(brand_name: 'A Jato', corporate_name: 'A Jato S.A.',
+                                email_domain: 'ajato.com', registration_number: 19_824_380_000_107,
+                                address: 'Avenida B, 23', city: 'Natal', state: 'RN')
+        invalid_user = User.new(email: 'usuario@email.com')
+        first_valid_user = User.new(email: 'usuario@express.com.br')
+        second_valid_user = User.new(email: 'usuario@ajato.com')
+
+        [invalid_user, first_valid_user, second_valid_user].each(&:valid?)
+
+        expect(invalid_user.errors[:email]).to include 'não possui um domínio registrado'
+        expect(first_valid_user.errors.include?(:email)).to eq false
+        expect(second_valid_user.errors.include?(:email)).to eq false
+      end
+    end
+  end
 end
