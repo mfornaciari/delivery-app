ee04d3a8e132e48152bba9f4d810d536fcf4f721
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-26 20:43:57 -0300

    refactor: melhora código de busca de preço Extrai funções e simplifica consultas visando legibilidade

--- app/controllers/budget_searches_controller.rb
+++ app/controllers/budget_searches_controller.rb
@@ -29,28 +29,34 @@ class BudgetSearchesController < ApplicationController
 
   def prices_and_times(search)
     price_time_hash = {}
-    possible_weight_ranges(search).each do |wrange|
+    valid_weight_ranges(search).each do |wrange|
       company = wrange.volume_range.shipping_company
-      min_price = company.price_distance_ranges.find_by(
-        'min_distance <= :distance AND max_distance >= :distance', distance: search.distance
-      )&.value
-      price = wrange.value * search.distance
-      price = min_price && price < min_price ? min_price : price
-      delivery_time = company.time_distance_ranges.find_by(
-        'min_distance <= :distance AND max_distance >= :distance', distance: search.distance
-      )&.delivery_time
+      delivery_time = company_delivery_time(company, search)
       next unless delivery_time
 
+      min_price = company_min_price(company, search)
+      price = wrange.value * search.distance
+      price = min_price && price < min_price ? min_price : price
       price_time_hash[company] = [price, delivery_time]
     end
     price_time_hash
   end
 
-  def possible_weight_ranges(search)
-    WeightRange.where(
-      'min_weight <= :weight AND max_weight >= :weight', weight: search.weight
-    ).joins(:volume_range).where(
-      'volume_ranges.min_volume <= :volume AND volume_ranges.max_volume >= :volume', volume: search.volume
+  def valid_weight_ranges(search)
+    WeightRange.joins(:volume_range).where(
+      'min_weight <= :weight AND max_weight >= :weight AND
+      volume_ranges.min_volume <= :volume AND volume_ranges.max_volume >= :volume',
+      weight: search.weight, volume: search.volume
     )
+  end
+
+  def company_delivery_time(company, search)
+    company.time_distance_ranges.find_by('min_distance <= :distance AND max_distance >= :distance',
+                                         distance: search.distance)&.delivery_time
+  end
+
+  def company_min_price(company, search)
+    company.price_distance_ranges.find_by('min_distance <= :distance AND max_distance >= :distance',
+                                          distance: search.distance)&.value
   end
 end
