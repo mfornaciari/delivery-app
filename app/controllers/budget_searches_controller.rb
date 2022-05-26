e8df3642543a9e718e01f8559a3cfd6ed24cff88
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-26 17:40:28 -0300

    feat: cria mensagem p/ buscas malsucedidas Buscas que não encontram transportadoras, não buscas c/ dados inválidos

--- app/controllers/budget_searches_controller.rb
+++ app/controllers/budget_searches_controller.rb
@@ -23,22 +23,29 @@ class BudgetSearchesController < ApplicationController
   end
 
   def prices_and_times(search)
-    weight_ranges = WeightRange.where('min_weight <= :weight AND max_weight >= :weight', weight: search.weight)
-    weight_ranges = weight_ranges.select do |wrange|
-      search.volume.between?(wrange.volume_range.min_volume, wrange.volume_range.max_volume)
-    end
-
     price_time_hash = {}
-    weight_ranges.each do |wrange|
+    possible_weight_ranges(search).each do |wrange|
       company = wrange.volume_range.shipping_company
-      min_price = company.price_distance_ranges.find_by('min_distance <= :distance AND max_distance >= :distance',
-                                                        distance: search.distance).value
+      min_price = company.price_distance_ranges.find_by(
+        'min_distance <= :distance AND max_distance >= :distance', distance: search.distance
+      )&.value
       price = wrange.value * search.distance
-      price = price < min_price ? min_price : price
-      delivery_time = company.time_distance_ranges.find_by('min_distance <= :distance AND max_distance >= :distance',
-                                                           distance: search.distance).delivery_time
+      price = min_price && price < min_price ? min_price : price
+      delivery_time = company.time_distance_ranges.find_by(
+        'min_distance <= :distance AND max_distance >= :distance', distance: search.distance
+      )&.delivery_time
+      next unless delivery_time
+
       price_time_hash[company] = [price, delivery_time]
     end
     price_time_hash
+  end
+
+  def possible_weight_ranges(search)
+    WeightRange.where(
+      'min_weight <= :weight AND max_weight >= :weight', weight: search.weight
+    ).joins(:volume_range).where(
+      'volume_ranges.min_volume <= :volume AND volume_ranges.max_volume >= :volume', volume: search.volume
+    )
   end
 end
--- spec/system/budget_searches/admin_budgets_order_spec.rb
+++ spec/system/budget_searches/admin_budgets_order_spec.rb
@@ -80,4 +80,39 @@ describe 'Administrador consulta preço de pedido' do
       end
     end
   end
+
+  it 'e não há transportadoras para atendê-lo' do
+    admin = Admin.create!(email: 'admin@sistemadefrete.com.br', password: 'password')
+    express = ShippingCompany.create!(brand_name: 'Express', corporate_name: 'Express Transportes Ltda.',
+                                      email_domain: 'express.com.br', registration_number: 28_891_540_000_121,
+                                      address: 'Avenida A, 10', city: 'Rio de Janeiro', state: 'RJ')
+    TimeDistanceRange.create!(shipping_company: express, min_distance: 0, max_distance: 100, delivery_time: 2)
+    first_express_volume_range = VolumeRange.create!(shipping_company: express, min_volume: 0, max_volume: 50)
+    WeightRange.create!(volume_range: first_express_volume_range, min_weight: 21, max_weight: 40, value: 50)
+    second_express_volume_range = VolumeRange.create!(shipping_company: express, min_volume: 51, max_volume: 100)
+    WeightRange.create!(volume_range: second_express_volume_range, min_weight: 0, max_weight: 20, value: 75)
+    a_jato = ShippingCompany.create!(brand_name: 'A Jato', corporate_name: 'A Jato S.A.',
+                                     email_domain: 'ajato.com', registration_number: 19_824_380_000_107,
+                                     address: 'Avenida B, 23', city: 'Natal', state: 'RN')
+    TimeDistanceRange.create!(shipping_company: a_jato, min_distance: 101, max_distance: 200, delivery_time: 3)
+    a_jato_volume_range = VolumeRange.create!(shipping_company: a_jato, min_volume: 0, max_volume: 50)
+    WeightRange.create!(volume_range: a_jato_volume_range, min_weight: 0, max_weight: 20, value: 50)
+
+    login_as(admin, scope: :admin)
+    visit new_budget_search_path
+    fill_in 'Altura', with: 100
+    fill_in 'Largura', with: 100
+    fill_in 'Profundidade', with: 100
+    fill_in 'Peso do item (em quilogramas)', with: 5
+    fill_in 'Distância a percorrer (em quilômetros)', with: 20
+    click_on 'Consultar'
+
+    expect(page).to have_content 'Resultado da sua busca:'
+    expect(page).to have_content "Busca no. 1 (#{I18n.l(Date.current)})"
+    expect(page).to have_content "Realizada por: #{admin.email}"
+    expect(page).to have_content 'Volume: 1 m³'
+    expect(page).to have_content 'Peso: 5 kg'
+    expect(page).to have_content 'Distância a percorrer: 20 km'
+    expect(page).to have_content 'Não foram encontradas transportadoras para esse pedido.'
+  end
 end
