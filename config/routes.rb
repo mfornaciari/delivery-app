10cf967aaf8ed6d166145dc3c902272c83522bb3
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-29 20:08:43 -0300

    refactor: simplifica código p/ criação de pedido

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,8 @@ Rails.application.routes.draw do
 
   resources :budget_searches, only: %i[new create show]
   resources :orders, only: %i[index new create show] do
-    get 'search', on: :collection
-
     resources :route_updates, only: :create
+    get 'search', on: :collection
     post 'accepted', on: :member
     post 'rejected', on: :member
     post 'finished', on: :member
