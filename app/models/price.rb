f5159fd1bbc8e9a3980eb108ae01945f16eaed27
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-03 18:52:36 -0300

    feat: validate numericality on Price attributes

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,5 @@
 class Price < ApplicationRecord
   validates :min_volume, :max_volume, :min_weight, :max_weight, :value, presence: true
+  validates :min_volume, :min_weight, :value, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_volume, :max_weight, numericality: { greater_than: 0 }
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Price, type: :model do
   it { is_expected.to validate_presence_of(:min_weight).with_message('não pode ficar em branco') }
   it { is_expected.to validate_presence_of(:max_weight).with_message('não pode ficar em branco') }
   it { is_expected.to validate_presence_of(:value).with_message('não pode ficar em branco') }
+
+  it { is_expected.to validate_numericality_of(:min_volume).is_greater_than_or_equal_to(0) }
+  it { is_expected.to validate_numericality_of(:max_volume).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:min_weight).is_greater_than_or_equal_to(0) }
+  it { is_expected.to validate_numericality_of(:max_weight).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
 end
